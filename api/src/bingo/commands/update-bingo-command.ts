1d6958f1bcf3bef0749289c0a0c6f7120a11b8e3
charozoid <directfromquebec@gmail.com>
2025-03-23 14:01:26 -0400

    Change queries, commands, events to 1 file only. Add authorization verification in UpdateBingo command and SearchBingos query.

--- api/src/bingo/commands/update-bingo-command.ts
+++ api/src/bingo/commands/update-bingo-command.ts
@@ -1,20 +1,131 @@
-import { Command } from '@nestjs/cqrs';
+import { Command, QueryBus } from '@nestjs/cqrs';
 
 import { type User } from '@/user/user.entity';
 
-import { type Bingo } from '../bingo.entity';
+import { Bingo } from '../bingo.entity';
 import { type UpdateBingoDto } from '../dto/update-bingo.dto';
 
+import { BadRequestException, NotFoundException, UnauthorizedException } from '@nestjs/common';
+import { CommandHandler } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { I18nService } from 'nestjs-i18n';
+import { Repository } from 'typeorm';
+
+import { I18nTranslations } from '@/i18n/types';
+import { GetBingoParticipantsQuery } from '@/bingo-participant/queries/get-bingo-participants.query';
+import { userHasRole } from '@/auth/roles/roles.utils';
+import { Roles } from '@/auth/roles/roles.constants';
+
 export type UpdateBingoParams = {
-  requester: User;
   bingoId: number;
-  updateBingoDto: UpdateBingoDto;
+  requester: User;
+  updates: {
+    language?: string;
+    title?: string;
+    description?: string;
+    isPrivate?: boolean;
+    width?: number;
+    height?: number;
+    fullLineValue?: number;
+    startDate?: Date;
+    endDate?: Date;
+  }
+
 };
 
 export type UpdateBingoResult = Bingo;
 
 export class UpdateBingoCommand extends Command<Bingo> {
-  constructor(public readonly params: UpdateBingoParams) {
+  public readonly bingoId: number;
+  public readonly requester: User;
+  public readonly updates: {
+    language?: string;
+    title?: string;
+    description?: string;
+    isPrivate?: boolean;
+    width?: number;
+    height?: number;
+    fullLineValue?: number;
+    startDate?: Date;
+    endDate?: Date;
+  }
+  constructor({bingoId, requester, updates}) {
     super();
+    this.bingoId = bingoId;
+    this.requester = requester;
+    this.updates = updates;
+  }
+}
+
+@CommandHandler(UpdateBingoCommand)
+export class UpdateBingoHandler {
+  constructor(
+    @InjectRepository(Bingo)
+    private readonly bingoRepository: Repository<Bingo>,
+    private readonly i18nService: I18nService<I18nTranslations>,
+    private readonly queryBus: QueryBus
+  ) {}
+
+  async execute(command: UpdateBingoCommand): Promise<UpdateBingoResult> {
+    const { bingoId, requester } =
+    command;
+
+    let bingo = await this.bingoRepository.findOneBy({ id: bingoId });
+
+    const requesterIsModerator = userHasRole(requester, Roles.Moderator);
+
+    if (!bingo) {
+      throw new NotFoundException(this.i18nService.t('bingo.updateBingo.bingoNotFound'));
+    }
+
+    const bingoParticipants = await this.queryBus.execute(new GetBingoParticipantsQuery({ bingoId: bingo.id }));
+
+    const participant = bingoParticipants.find((participant) => {
+      return participant.userId === requester.id;
+    });
+
+    if (!requesterIsModerator && !participant) {
+      throw new UnauthorizedException(this.i18nService.t('bingo.updateBingo.notAuthorized'));
+    }
+
+    if (!requesterIsModerator && bingo.startedAt) {
+      throw new BadRequestException(this.i18nService.t('bingo.updateBingo.bingoNotPending'));
+    }
+
+    const updates = Object.fromEntries(
+      Object.entries(command.updates).filter(([key, value]) => value !== undefined && value !== bingo![key]),
+    ) as UpdateBingoParams['updates'];
+
+    if (updates.description) {
+      bingo.description = updates.description;
+    }
+
+    if (updates.startDate) {
+      bingo.startDate = updates.startDate;
+    }
+
+    if (updates.endDate) {
+      bingo.endDate = updates.endDate;
+    }
+
+    if (updates.language) {
+      bingo.language = updates.language;
+    }
+
+    if (updates.isPrivate) {
+      bingo.private = updates.isPrivate;
+    }
+
+    if (updates.title) {
+      bingo.title = updates.title;
+    }
+
+    if (updates.fullLineValue) {
+      bingo.fullLineValue = updates.fullLineValue;
+    }
+
+    bingo = await this.bingoRepository.save(bingo);
+
+    return bingo;
   }
 }
--- api/src/bingo/dto/update-bingo.dto.ts
+++ api/src/bingo/dto/update-bingo.dto.ts
@@ -4,40 +4,25 @@ import { IsBoolean, IsDate, IsNotEmpty, IsNumber, IsPositive, IsString } from 'c
 
 export class UpdateBingoDto {
   @ApiProperty()
-  @IsNotEmpty()
-  @IsString()
   language: string;
 
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   title: string;
 
   @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
   description: string;
 
   @ApiProperty()
-  @IsBoolean()
-  @IsNotEmpty()
   private: boolean;
 
   @ApiProperty()
-  @IsNumber()
-  @IsPositive()
-  @IsNotEmpty()
   fullLineValue: number;
 
   @ApiProperty()
-  @IsDate()
-  @IsNotEmpty()
   @Type(() => Date)
   startDate: Date;
 
   @ApiProperty()
-  @IsDate()
-  @IsNotEmpty()
   @Type(() => Date)
   endDate: Date;
 }
